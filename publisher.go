8eb07094d897a83951515eaee651ce649f595028
K!a <kialakunngalgal@gmail.com>
2022-08-02 18:39:47 +1000

    Update publisher.go

--- publisher.go
+++ publisher.go
@@ -2,6 +2,7 @@ package pubsub
 
 // publishes data to a topic by sending it to all registered subscriber channels
 func (e *EventBus) Publish(topic string, data interface{}) {
+	// @todo: implement mutext to safely read/write from EventBus
 	if subscribers, found := e.subscribers[topic]; found {
 		for _, subscriber := range subscribers {
 			subscriber <- Event{Data: data, Topic: topic}
