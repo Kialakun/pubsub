abab8b643b82d8c8fd18fb42fb1f86ceacfb82a0
K!a <kialakunngalgal@gmail.com>
2022-08-02 18:38:23 +1000

    Update subscriber.go

--- subscriber.go
+++ subscriber.go
@@ -2,9 +2,10 @@ package pubsub
 
 // subscribes to a topic and returns the channel to listen on
 func (e *EventBus) Subscribe(topic string) *EventChannel {
+	// @todo: implement mutex to safely read/write to EventBus
 	channel := make(EventChannel)
-	if subscribers, found := e.subscribers[topic]; found {
-
+	if _, found := e.subscribers[topic]; found {
+		e.subscribers[topic] = append(e.subscribers[topic], channel)
 	} else {
 		e.subscribers[topic] = append(EventChannels{}, channel)
 	}
